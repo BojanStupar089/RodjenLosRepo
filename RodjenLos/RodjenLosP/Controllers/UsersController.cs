f6bfc16e4ebdc4946b0062c58efb699a3317e4f8
BojanStupar089 <bojanstupar089@gmail.com>
2025-10-05 13:05:09 +0200

    init.

---
+++ RodjenLos/RodjenLosP/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RodjenLosP.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Dje se kupas");
+        }
+    }
+}
